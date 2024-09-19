85248107786d7654d045a113c15a01a1ff34e601
alebuld <80888358+alebuld@users.noreply.github.com>
2024-09-19 09:32:04 +0500

    dob

---
+++ AV/src/three.java
@@ -0,0 +1,14 @@
+
+public class three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 18;
+		
+		System.out.print("Юлиана ");
+		System.out.println("18");
+		
+		System.out.println("Лена " + age);
+	}
+
+}
---
+++ AV/src/two.java
@@ -0,0 +1,14 @@
+
+public class two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Раз");
+		System.out.println("Два");
+		System.out.println("Три");
+		System.out.println("Четыре");
+		System.out.println("Пять");
+		System.out.println("Шесть");
+	}
+
+}
